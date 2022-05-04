fe0cb1db0260fd1a1844d734cc597155abc2b953
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-05-04 12:03:22 -0300

    remodeling the model

--- src/model/universities.js
+++ src/model/universities.js
@@ -4,20 +4,22 @@ const schema = new mongoose.Schema({
     alpha_two_code:{
         type: String,
     },
-    domains: [{
-        type: String,
-    }],
-    country: {
-        type: String,
-    },
-    state_province: {
-        type: String
-    },
     web_pages: [{
         type: String,
     }],
     name: {
         type: String,
+        unique: true
+    },
+    country: {
+        type: String,
+        unique: true
+    },
+    domains: [{
+        type: String,
+    }],
+    state_province: {
+        type: String
     },
 })
 
