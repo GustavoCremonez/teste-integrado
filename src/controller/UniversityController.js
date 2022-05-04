810b3e6537aeac68b7dab988bc41ce8ffdfa8f3c
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-05-04 11:02:27 -0300

    fixing get method to add get method by id

--- src/controller/UniversityController.js
+++ src/controller/UniversityController.js
@@ -1,25 +1,27 @@
 const UniversityModel = require('../model/universities')
 
 async function get(req, res){
-    const { id, country} = req.params
+    const { country } = req.params
 
-    const filterId = id ? { _id: id} : null
     const filterCountry = country ? { country: country} : null
 
-    if(filterId){
-        const university = await UniversityModel.findOne(filterId)
-        res.send(university)
-    }
-    else if(filterCountry){
-        const universities = await UniversityModel.find(filterCountry)
-        res.send(universities)
-    }
-    else{
-        const universities = await UniversityModel.find()
-        res.send(universities)
-    }
+    const universities = await UniversityModel.find(filterCountry)
+    res.send(universities)
+}
+
+async function getById(req, res){
+    const { id } = req.params
+
+    const university = await UniversityModel.findOne(id)
+    res.send(university)
+}
+
+async function post(req, res){
+
 }
 
 module.exports = {
     get,
+    getById,
+    post,
 }
