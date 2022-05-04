29036622d0f25c4882048888145a9fd7be83e659
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-05-04 12:12:19 -0300

    adding method put

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,6 @@ const UniversityController = require('../controller/UniversityController')
 router.get('/universities/:country?', UniversityController.get)
 router.get('/universities/:id', UniversityController.getById)
 router.post('/universities', UniversityController.post)
+router.put('/universities/:id', UniversityController.put)
 
 module.exports = router
