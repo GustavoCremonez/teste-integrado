020de9e708774ab25a8a088053d01ccbeaefceda
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-05-04 12:03:48 -0300

    adding post method

--- src/controller/UniversityController.js
+++ src/controller/UniversityController.js
@@ -1,23 +1,35 @@
 const UniversityModel = require('../model/universities')
 
-async function get(req, res){
+async function get(req, res) {
     const { country } = req.params
 
-    const filterCountry = country ? { country: country} : null
+    const filterCountry = country ? { country: country } : null
 
     const universities = await UniversityModel.find(filterCountry)
     res.send(universities)
 }
 
-async function getById(req, res){
+async function getById(req, res) {
     const { id } = req.params
 
     const university = await UniversityModel.findOne(id)
     res.send(university)
 }
 
-async function post(req, res){
-
+async function post(req, res) {
+    const university = new UniversityModel(req.body)
+    
+    university.save()
+        .then(() => {
+            res.send({
+                message: 'success'
+            })
+        })
+        .catch((error) => {
+            res.send({
+                message: `${error}`
+            })
+        })
 }
 
 module.exports = {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 const UniversityController = require('../controller/UniversityController')
 
-router.get('/universities/:id?:country?', UniversityController.get)
+router.get('/universities/:country?', UniversityController.get)
+router.get('/universities/:id', UniversityController.getById)
+router.post('/universities', UniversityController.post)
 
 module.exports = router
