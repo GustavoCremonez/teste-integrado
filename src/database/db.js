0e8a038b28cf9c8f483f7b5d4542e34f546aba82
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-05-06 11:54:07 -0300

    adding new env to database

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+const db_host = process.env.DB_HOST
+const db_port = process.env.DB_PORT
+const db_name = process.env.DB_NAME
 
 function connect() {
-	mongoose.connect('mongodb://localhost:27017/testIntegrado')
+	mongoose.connect(`mongodb://${db_host}:${db_port}/${db_name}`)
 
 	const db = mongoose.connection
 
